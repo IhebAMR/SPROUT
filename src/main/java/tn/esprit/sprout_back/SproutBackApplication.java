be6349f43a4b99a146ef21eba8d29beed77f09f6
Iheb <amri.iheb@esprit.tn>
2025-02-20 19:13:44 +0100

    mvc architecture

---
+++ src/main/java/tn/esprit/sprout_back/SproutBackApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.sprout_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproutBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SproutBackApplication.class, args);
+	}
+
+}
